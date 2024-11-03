1717f1c374293e89da28cb1a7133a471d6ac6fe2
Brian Tipton <briantiptondev@gmail.com>
2024-11-03 11:09:33 -0600

    fix multi

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,10 +1,11 @@
 local P = {
   'mg979/vim-visual-multi',
-  init = function() end,
-  vim.cmd [[
+  init = function()
+    vim.cmd [[
   let g:VM_maps = {}
   let g:VM_maps["Add Cursor Down"]             = '<C-A-Down>'
   let g:VM_maps["Add Cursor Up"]               = '<C-A-Up>'
-]],
+]]
+  end,
 }
 return P
